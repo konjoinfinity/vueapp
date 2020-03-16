7046ff68ee1f1760fd6c7c0ab8e4d228e3251b6e
Wesley Scholl <clients@konjoweb.com>
2020-03-16 18:41:55 -0400

    still building out the nav and routing, links are not active yet

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -3,18 +3,19 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <Home msg="News App" />
     <router-view />
+    <router-link to="/home">Home</router-link>
+    <router-link to="/news">News</router-link>
     <Navigation></Navigation>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
+// import Home from "./components/Home.vue";
 import Navigation from "./components/Nav";
 
 export default {
   name: "App",
   components: {
-    Home,
     Navigation
   }
 };
