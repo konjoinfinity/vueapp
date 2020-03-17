b7fdb870aa6b6fcc872b6548bd219130c594f7a7
Wesley Scholl <clients@konjoweb.com>
2020-03-17 08:49:41 -0400

    removed unused components and code

--- vueapp/src/components/Nav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h3>Header</h3>
-    <nav>
-      <router-link to="/home">Home</router-link>
-      <router-link to="/news">News</router-link>
-    </nav>
-  </div>
-</template>
