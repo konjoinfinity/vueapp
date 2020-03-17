5489aef106925ec3a1b7a3a509d709b69e59d2d9
Wesley Scholl <clients@konjoweb.com>
2020-03-17 09:27:41 -0400

    updated news article styling and text

--- vueapp/src/components/News.vue
+++ vueapp/src/components/News.vue
@@ -3,82 +3,173 @@
     <div class="jumbotron">
       <h2 class="display-3">News</h2>
     </div>
-    <div class="flex-row">
-      <div class="list-group">
-        <a
-          href="#"
-          class="list-group-item list-group-item-action flex-column align-items-start active"
-        >
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small>3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small>Donec id elit non mi porta.</small>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small class="text-muted">3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-      </div>
-      <div class="list-group">
-        <a
-          href="#"
-          class="list-group-item list-group-item-action flex-column align-items-start active"
-        >
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small>3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small>Donec id elit non mi porta.</small>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small class="text-muted">3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-      </div>
-      <div class="list-group">
-        <a
-          href="#"
-          class="list-group-item list-group-item-action flex-column align-items-start active"
-        >
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small>3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small>Donec id elit non mi porta.</small>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">List group item heading</h5>
-            <small class="text-muted">3 days ago</small>
-          </div>
-          <p
-            class="mb-1"
-          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-          <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-      </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #1</h5>
+          <small>Today</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">Today</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #2</h5>
+          <small>1 Day Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">1 Day Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #3</h5>
+          <small>2 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">2 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #4</h5>
+          <small>2 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">2 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #5</h5>
+          <small>3 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">3 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #6</h5>
+          <small>4 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">4 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #7</h5>
+          <small>4 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">4 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
+    </div>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start active"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">News article #8</h5>
+          <small>5 Days Ago</small>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Article Title</h5>
+          <small class="text-muted">5 Days Ago</small>
+        </div>
+        <p
+          class="mb-1"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small class="text-muted">Written by: Author</small>
+      </a>
     </div>
   </div>
 </template>
