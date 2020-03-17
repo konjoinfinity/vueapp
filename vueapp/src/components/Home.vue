073e76e23e8c9bbb0f35b3ac988abcca503b5c29
Wesley Scholl <clients@konjoweb.com>
2020-03-17 09:11:55 -0400

    added news feed

--- vueapp/src/components/Home.vue
+++ vueapp/src/components/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="home">
     <div class="jumbotron">
-      <h1 class="display-3">Hello, world!</h1>
-      <p
-        class="lead"
-      >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <h1 class="display-3">Welcome!</h1>
+      <p class="lead">View current events in real time and subscribe to stay even more in the loop.</p>
       <hr class="my-4" />
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <p class="lead">
--- vueapp/src/components/News.vue
+++ vueapp/src/components/News.vue
@@ -1,15 +1,84 @@
 <template>
   <div class="NewsPage">
     <div class="jumbotron">
-      <h1 class="display-3">Hello, world!</h1>
-      <p
-        class="lead"
-      >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <hr class="my-4" />
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-      <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </p>
+      <h2 class="display-3">News</h2>
+    </div>
+    <div class="flex-row">
+      <div class="list-group">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action flex-column align-items-start active"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small>3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small>Donec id elit non mi porta.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+      </div>
+      <div class="list-group">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action flex-column align-items-start active"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small>3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small>Donec id elit non mi porta.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+      </div>
+      <div class="list-group">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action flex-column align-items-start active"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small>3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small>Donec id elit non mi porta.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p
+            class="mb-1"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+      </div>
     </div>
   </div>
 </template>
