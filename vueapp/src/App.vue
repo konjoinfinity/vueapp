dce224b9419be1be2af451470b090043c17f4da2
Wesley Scholl <clients@konjoweb.com>
2020-03-17 09:05:48 -0400

    added bootswatch css, configuring header, nav, and views

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,9 +1,35 @@
 <template>
   <div id="app">
-    <h2>Konjo News</h2>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Konjo News</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/news">News</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <router-view></router-view>
-    <router-link to="/">Home</router-link>
-    <router-link to="/news">News</router-link>
   </div>
 </template>
 
@@ -15,13 +41,6 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src='./assets/bootstrap.min.css'>
 </style>
+
